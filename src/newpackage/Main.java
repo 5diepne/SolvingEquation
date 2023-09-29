0b31d2bf0df33ddf9f578f09ca3b4cec4f796cd6
DiepTCNN <DiepTCNN@DESKTOP-KHNR5UF>
2023-09-29 14:14:26 +0700

    SolvingEquation

---
+++ src/newpackage/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+import controller.EquationManagement;
+
+/**
+ *
+ * @author DiepTCNN
+ */
+public class Main {
+    public static void main(String[] args) {
+        new EquationManagement().run();
+    }
+}
